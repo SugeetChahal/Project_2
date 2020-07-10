477b17e090024169b57bc141a90ddec5cdd9e8eb
Nehemiah B. Dang <nehemiahdang@gmail.com>
2020-07-09 19:56:11 -0700

    Fixed app.py and added app.js

--- ND/app.py
+++ ND/app.py
@@ -1,5 +1,5 @@
 # Import Dependencies
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import scrape_covid
 
@@ -15,14 +15,24 @@ def home():
     covid_data = mongo.db.covid_data.find_one()
     return render_template("index.html", text="COVID Data", covid_data=covid_data)
 
-@app.route("/scrape")
-def scrape():
-    #Scrape for COVID data
-    covid_data = mongo.db.covid_data
-    covid_scrape_data = scrape_covid.scrape_data()
-    covid_data.update({}, covid_scrape_data, upsert=True)
 
-    return redirect("/", code=302)
+@app.route("/covidjson")
+def covidjson():
+    covid_data = mongo.db.covid_data.find_one()
+    clean_data = {}
+    for x in covid_data:
+        if x != "_id":
+            clean_data[x] = covid_data[x]
+    print(clean_data)
+    return jsonify(clean_data)
+
+# @app.route("/scrape")
+# def scrape():
+#     #Scrape for COVID data
+#     covid_data = mongo.db.covid_data
+#     covid_scrape_data = scrape_covid.scrape_data()
+#     covid_data.update({}, covid_scrape_data, upsert=True)
+#     return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
